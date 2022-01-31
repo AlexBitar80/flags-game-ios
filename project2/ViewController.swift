79491f69c3c06a7a4225750eac5c171d36501623
AlexBitar80 <joaoalexandre.bitar@gmail.com>
2022-01-31 18:52:41 -0300

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by João Alexandre on 31/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
